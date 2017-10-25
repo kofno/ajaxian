19d673faaf0b67cb981ceae9c46bc4992400cf3e
Dave Christiansen <dave@trooptrack.com>
2017-10-24 20:53:10 -0500

    Cuz sometimes I just want to show my methods

--- src/Request.ts
+++ src/Request.ts
@@ -1,16 +1,22 @@
-import { Result } from 'resulty';
+import { Result } from "resulty";
 
 export type DecoderFn<A> = (json: string) => Result<string, A>;
 
-export type Method
-  = 'get'
-  | 'post'
-  | 'put'
-  | 'patch'
-  | 'head'
-  | 'options'
-  | 'delete'
-  ;
+export type Method =
+  | "get"
+  | "GET"
+  | "post"
+  | "POST"
+  | "put"
+  | "PUT"
+  | "patch"
+  | "PATCH"
+  | "head"
+  | "HEAD"
+  | "options"
+  | "OPTIONS"
+  | "delete"
+  | "DELETE";
 
 export interface Request<A> {
   url: string;
